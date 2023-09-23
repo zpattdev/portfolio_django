72164ca5ed5037cb0d473cda97aa0a349e6d8ca1
zack <zpattdev@gmail.com>
2023-09-23 10:59:43 -0400

    add base.html to ref bootstrap (bug: title block not rendering)

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [
+            BASE_DIR / "templates/",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
